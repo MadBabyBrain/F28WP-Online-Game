457ce0c1ff3a5b5b8566d2b15e1e020afe0ce62f
Jakubyte <jakubdymar@gmail.com>
2020-09-23 17:42:19 +0100

    Added POST, PUT and DELETE functionality
    Installed module Joi for schema validation
    POST request functionailty
    PUT request functionailty
    DELET request funcionailty

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -1,5 +1,9 @@
+const Joi = require("./API/node_modules/joi");
 const express = require("./API/node_modules/express");
 const app = express();
+
+// enable JSON parsing - not enable in express by default
+app.use(express.json());
 
 //  PORT
 const port = process.env.PORT || 3000;
@@ -11,41 +15,128 @@ app.get('/', (req, res) => {
     res.send("Hello world!");
 });
 
-// test object
-var scores = {
-    "name":["sample1", "sample2"],
-    "score":[1,2]
-};
+// test array of objects
+const scores = [
+    {"id":0, "name":"user1", "score":1},
+    {"id":1, "name":"user2", "score":2},
+    {"id":2, "name":"user3", "score":3}
+];
 
 app.get('/api/scores', (req, res) => {
-    res.send(JSON.stringify(scores));
+    res.send(scores);
 });
 
 // /api/scores/1
 app.get('/api/scores/:id', (req, res) => {
-    var id = req.params.id;
-    res.send(`Name: ${scores.name[id]}
-    Score: ${scores.score[id]}`);
+    let id = parseInt(req.params.id);
+    const score = scores.find(c => c.id === id);
+    // 404
+    if(!score) return res.status(404).send("The score with the given ID was not found");
+    //else
+    res.send(score);
 });
 
-// /api/date/dd/mm/yyyy
-app.get('/api/date/:day/:month/:year', (req, res) => {
-    res.send(req.params);
-    // query params
-    // res.send(req.query);
+// reponding to post from HTML
+app.post('/api/scores', (req, res) => {
+    // validate
+    //const result = validateScore(req.body);
+    const { error } = validateScore(req.body); // object destructuring - result.error
+
+    // ! Replaced by Joi module for efficeint post validation !
+    // look up how to validate post data more efficiently
+    // if(!req.body.name || req.body.name.length < 3) {
+    //     // 400 - bad request
+    //     res.status(400).send("Name is required - minimum 3 characters");
+    //     return;
+    // }
+
+    // if(!req.body.score) {
+    //     // 400 - bad request
+    //     res.status(400).send("Score is required");
+    //     return;
+    // }
+
+    if(error) {
+        // 400 - bad request
+        // res.status(400).send(result.error);
+        return res.status(400).send(error.details[0].message); //easier for client to read
+    }
+
+/* 
+    try {
+        const result = schema.validate(req.body);
+        console.log(result);
+    } catch (err) {
+        // 400 - bad request
+        res.status(400).send(err);
+    } */
+
+    const score = { // maunually added data. Future revisions will let MongoDB populate object
+        "id": scores.length +1,
+        "name": req.body.name,
+        "score": req.body.score
+    };
+
+    scores.push(score);
+    res.send(score);
 });
 
-// query params
-// /api/date/dd/mm/yyyy?sortBy=name
-// app.get('/api/date/:day/:month/:year', (req, res) => {
-//     res.send(req.query);
-// });
+// dealing with put requests/updating resources
+app.put('/api/scores/:id', (req, res) => {
+    // Look up the course
+    // If not exist - 404 - not found
+    let id = parseInt(req.params.id);
+    const score = scores.find(c => c.id === id);
+    if(!score) return res.status(404).send("The score with the given ID was not found");
 
+    // else
+    // Validate
+    const { error } = validateScore(req.body);
+    
+    // if invalid - 400 - bad request
+    if(error) {
+        // 400 - bad request
+        // res.status(400).send(result.error);
+        return res.status(400).send(error.details[0].message); //easier for client to read
+    }
 
+    // update
+    score.name = req.body.name;
+    score.score = req.body.score;
+    // return update
+    res.send(score);
 
-//
-// app.post();
-// app.put();
-// app.delete();
+});
+
+function validateScore(score) {
+    const schema = Joi.object({
+        "name": Joi.string()
+                .min(3)
+                .required(),
+
+        "score": Joi.number()
+                .integer()
+                .min(0)
+                .required()
+    });
+
+    return schema.validate(score);
+}
+
+// handling HTTP delete ops
+app.delete('/api/scores/:id', (req, res) => {
+    // Look up the course
+    // If not exist - 404 - not found
+    let id = parseInt(req.params.id);
+    const score = scores.find(c => c.id === id);
+    if(!score) return res.status(404).send("The score with the given ID was not found");
+
+    // Delete
+    const index = scores.indexOf(score);
+    scores.splice(index, 1);
+
+    // return the same score
+    res.send(score); 
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));

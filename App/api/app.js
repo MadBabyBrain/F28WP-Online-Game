23011677092904f6e53d8dd30e93ac10b6926ff2
Jakubyte <jakubdymar@gmail.com>
2020-10-13 14:15:22 +0100

    Added basic error handling
    Added morgan for logging HTTP Requests to console
    
    Added very basic error handling

--- App/api/app.js
+++ App/api/app.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const userRoutes = require('./routes/user/users');
 const loginRoute = require('./routes/user/account/login');
 const signupRoute = require('./routes/user/account/signup');
 const scoreRoute = require('./routes/score/scoreboard');
 
+app.user(morgan('dev'));
+
 app.use('/users', userRoutes);
 app.use('/users/account/login', loginRoute);
 app.use('/users/account/signup', signupRoute);
-
 app.use('/score', scoreRoute);
+
+app.use((req, res, next) => {
+    const err = new Error("Not found :(");
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500); // 500 - all other kinds of errors
+    res.json({
+        error : {
+            message : err.message
+        }
+    });
+});
 
 module.exports = app;

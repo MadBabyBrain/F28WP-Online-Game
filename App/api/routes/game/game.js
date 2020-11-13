3a3b39ff6f737f9b2a45b0485b49b7cb00dd7ac0
MadBabyBrain <46905566+MadBabyBrain@users.noreply.github.com>
2020-11-13 17:00:41 +0000

    Added a get request to send game file

--- App/api/routes/game/game.js
+++ App/api/routes/game/game.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const resolve = require('path').resolve
 
-
+router.get('/', (req, res, next) => {
+    res.sendFile(resolve('../../App/public/html/Game.start.html'));
+});
 
 module.exports = router;

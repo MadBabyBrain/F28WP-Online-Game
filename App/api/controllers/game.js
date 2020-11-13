444c5b5e8b0cea549b3115b1cf111ba2a8a13337
MadBabyBrain <46905566+MadBabyBrain@users.noreply.github.com>
2020-11-13 17:00:16 +0000

    Added socket test

--- App/api/controllers/game.js
+++ App/api/controllers/game.js
@@ -1,12 +1,16 @@
+const { SSL_OP_NO_TICKET } = require('constants');
 const express = require('express');
 const router = express.Router();
 const app = express();
-const http = require("http").createServer(app);
+const http = require('http').createServer(app);
 const io = require("socket.io")(http);
-
 
 io.on('connection', (socket) => {
     console.log('a user connected');
 })
+
+io.on('move', (socket) => {
+    console.log('Test')
+});
 
 module.exports = router;

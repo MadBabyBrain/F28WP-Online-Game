6c198eb3a0147396d119ad403f0c3dc6fc5e01c8
Jakubyte <jakubdymar@gmail.com>
2020-11-16 16:24:38 +0000

    Updated app.js for merge

--- App/api/app.js
+++ App/api/app.js
@@ -1,62 +1,61 @@
 const dotenv = require('dotenv').config();
-const express = require('express');
-const app = express();
-const morgan = require('morgan');
-const bodyparser = require('body-parser');
-const mongoose = require('mongoose');
+const resolve = require('path').resolve
 
 const userRoutes = require('./routes/user/users');
 const scoreRoute = require('./routes/score/scoreboard');
 const homeRoute = require('./routes/home/home');
 const gameRoute = require('./routes/game/game');
 
-app.use(morgan('dev'));
-app.use(bodyparser.urlencoded({extended: false}));
-app.use(bodyparser.json());
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
 
-app.use('/users', userRoutes);
-app.use('/score', scoreRoute);
-app.use('/home', homeRoute);
-app.use('/game', gameRoute);
+module.exports = (app) => {
 
-mongoose.connect(
-    process.env.DB_HOST, 
-    {
-        useNewUrlParser: true, 
-        useUnifiedTopology: true
-    }
-);
+    app.use(morgan('dev'));
+    app.use(bodyparser.urlencoded({ extended: false }));
+    app.use(bodyparser.json());
 
-mongoose.Promise = global.Promise;
+    app.use('/users', userRoutes);
+    app.use('/score', scoreRoute);
+    app.use('/home', homeRoute);
+    app.use('/game', gameRoute);
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    if (req.method === "OPTIONS") {
-      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-      return res.status(200).json({});
-    }
-    next();
-  });
-
-// TODO : Handle Requests here
-
-app.use((req, res, next) => {
-    const err = new Error("Not found :(");
-    err.status = 404;
-    next(err);
-});
-
-app.use((err, req, res, next) => {
-    res.status(err.status || 500); // 500 - all other kinds of errors
-    res.json({
-        error : {
-            message : err.message
-        }
+    app.get('/socket.io.js', (req, res, next) => {
+        res.sendFile(resolve('./node_modules/socket.io/client-dist/socket.io.min.js'));
     });
-});
 
-module.exports = app;
+    app.get('/socket.io.min.js.map', (req, res, next) => {
+        res.sendFile(resolve('./node_modules/socket.io/client-dist/socket.io.min.js.map'));
+    });
+
+    app.get('/images/player_placeholder.png', (req, res, next) => {
+        res.sendFile(resolve('../public/images/player_placeholder.png'));
+    });
+
+    app.get('/css/player.css', (req, res, next) => {
+        res.sendFile(resolve('../public/css/player.css'));
+    });
+
+    app.get('/js/Game_Logic.js', (req, res, next) => {
+        res.sendFile(resolve('../public/js/Game_Logic.js'));
+    });
+    app.get('/js/Player_Controller.js', (req, res, next) => {
+        res.sendFile(resolve('../public/js/Player_Controller.js'));
+    });
+
+
+    app.use((req, res, next) => {
+        const err = new Error("Not found :(");
+        err.status = 404;
+        next(err);
+    });
+
+    app.use((err, req, res, next) => {
+        res.status(err.status || 500); // 500 - all other kinds of errors
+        res.json({
+            error: {
+                message: err.message
+            }
+        });
+    });
+}

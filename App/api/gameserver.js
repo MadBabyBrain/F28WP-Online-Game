2a2a039ae209592006fdf36f1324519634fa23f7
Jakubyte <jakubdymar@gmail.com>
2020-11-19 20:49:08 +0000

    Socket.io working
    Can connect
    Can disconnect
    See other players join/disconnect
    See other players move
    You can move youself too
    YAY!

--- App/api/gameserver.js
+++ App/api/gameserver.js
@@ -1,9 +1,34 @@
+let gameState = {};
 module.exports = (io) => {
     io.on('connection', (socket) => {
-        console.log('a user connected');
+        console.log('a user connected ' + socket.id);
 
-        socket.on('move', (position) => {
-            console.log('X: ' + position.positionX + ' Y: ' + position.positionY);
+        socket.emit('load_players', gameState);
+
+        socket.on('create_player', (player, id) => {
+            gameState[id] = {
+                x : player.positionX,
+                y : player.positionY
+            };
+
+            socket.broadcast.emit('load_player', id, gameState);
+
+            //console.log(gameState);
+        });
+
+        socket.on('move', player => {
+            //console.log('X: ' + player.x + ' Y: ' + player.y);
+            gameState[player.id] = player;
+
+            socket.broadcast.emit('update_players', player.id, {x : player.x, y : player.y});
+        });
+
+        socket.on('disconnect', reason => {
+            console.log("player " + socket.id + " disconnected...");
+            const id = socket.id;
+            delete gameState[socket.id];
+            console.log(gameState);
+            socket.broadcast.emit('remove_player', id);
         });
 
     });
--- App/public/js/Player_Controller.js
+++ App/public/js/Player_Controller.js
@@ -1,23 +1,85 @@
+const socket = io();
+const Player = {
+  id : "",
+  x : 0,
+  y : 0,
+  sprite : "",
+  ref : null
+};
+
+const interactables = document.getElementsByClassName("interactable");
+console.log(interactables);
+const interact_coords = {
+  rock : {},
+  paper : {},
+  scissors : {}
+};
+
+window.addEventListener("onbeforeunload", function() {
+  socket.disconnect();
+});
+
+socket.on('remove_player', id => {
+  document.getElementById(id).remove();
+})
+
+socket.on('load_players', gs => {
+  for (p in gs) {
+    const player = document.createElement('IMG');
+    player.setAttribute("class", "player");
+    player.setAttribute("id", p);
+    document.body.insertBefore(player, document.getElementById("buttons"));
+
+    //set initialise player
+    player.style.left = gs[p].x + "px";
+    player.style.top = gs[p].y + "px";
+  }
+});
+
+socket.on('update_players', (id, pos) => {
+  let player = document.getElementById(id);
+  player.style.left = pos.x + "px";
+  player.style.top = pos.y + "px";
+});
+
 let playerObj = {
   positionX: Math.floor(Math.random() * 600 + 100),
   positionY: Math.floor(Math.random() * 400 + 100),
   image: "../images/character_facing_down.png"
 }
 
-
 // var image = document.createElement("IMG")
 // image.src = image
 // player.appendChild(image)
 
+socket.on('load_player', (id, gs) => {
+  const player = document.createElement('IMG');
+  player.setAttribute("class", "player");
+  player.setAttribute("id", id);
+  document.body.insertBefore(player, document.getElementById("buttons"));
 
+  //set initialise player
+  player.style.left = gs[id].x + "px";
+  player.style.top = gs[id].y + "px";
+});
 
-const player = document.createElement('IMG')
-player.setAttribute("id", "player")
-document.body.insertBefore(player, document.getElementById("buttons"));
+function createPlayer() {
+  const player = document.createElement('IMG');
+  player.setAttribute("class", "player");
+  player.setAttribute("id", socket.id);
+  document.body.insertBefore(player, document.getElementById("buttons"));
 
-//set initialise player
-player.style.left = playerObj.positionX + "px";
-player.style.top = playerObj.positionY + "px";
+  //set initialise player
+  player.style.left = playerObj.positionX + "px";
+  player.style.top = playerObj.positionY + "px";
+
+  Player["id"] = socket.id;
+  Player["x"] = playerObj.positionX;
+  Player["y"] = playerObj.positionY;
+  Player["ref"] = document.getElementById(Player.id);
+
+  socket.emit('create_player', playerObj, socket.id);
+}
 
 
 
@@ -63,27 +125,22 @@ function movePlayer(key) {
 
   switch (key) {
     case 65:
-      playerObj.positionX = parseInt(player.style.left, 10) - movementSpeed
-      player.style.left = playerObj.positionX + "px"
+      Player.x = parseInt(Player.ref.style.left, 10) - movementSpeed
+      Player.ref.style.left = Player.x + "px"
       break
     case 68:
-      playerObj.positionX = parseInt(player.style.left, 10) + movementSpeed
-      player.style.left = playerObj.positionX + "px"
+      Player.x = parseInt(Player.ref.style.left, 10) + movementSpeed
+      Player.ref.style.left = Player.x + "px"
       break
     case 87:
-      playerObj.positionY = parseInt(player.style.top, 10) - movementSpeed
-      player.style.top = playerObj.positionY + "px"
+      Player.y = parseInt(Player.ref.style.top, 10) - movementSpeed
+      Player.ref.style.top = Player.y + "px"
       break
     case 83:
-      playerObj.positionY = parseInt(player.style.top, 10) + movementSpeed
-      player.style.top = playerObj.positionY + "px"
+      Player.y = parseInt(Player.ref.style.top, 10) + movementSpeed
+      Player.ref.style.top = Player.y + "px"
       break
   }
-  console.log(playerObj.positionX, playerObj.positionY);
-  updateposition(playerObj);
-};
-
-
-function displayPlayers() {
-
+  console.log(Player.x, Player.y);
+  socket.emit('move', Player);
 };

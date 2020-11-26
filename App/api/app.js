63c9bb0a3a10c800c955567fd4a3e376b6eb418a
MadBabyBrain <46905566+MadBabyBrain@users.noreply.github.com>
2020-11-26 16:00:43 +0000

    remove ./app from app

--- App/api/app.js
+++ App/api/app.js
@@ -19,67 +19,67 @@ module.exports = (app) => {
     app.use('/game', gameRoute);
 
     app.get('/', (req, res, next) => {
-        res.sendFile(resolve('./app/public/html/index.html'));
+        res.sendFile(resolve('./public/html/index.html'));
     })
 
     app.get('/favicon.ico', (req, res, next) => {
-        res.sendFile(resolve('./App/public/images/player_placeholder.png'));
+        res.sendFile(resolve('./public/images/player_placeholder.png'));
     });
 
     app.get('/gameroom', (req, res, next) => {
-        res.sendFile(resolve('./App/public/html/gamepage.html'));
+        res.sendFile(resolve('./public/html/gamepage.html'));
     });
 
     app.get('/html/:id', (req, res, next) => {
-        res.sendFile(resolve(`./app/public/html/${req.params.id}`));
+        res.sendFile(resolve(`./public/html/${req.params.id}`));
     });
 
     app.get("/images/:id", (req, res, next) => {
-        res.sendFile(resolve(`./app/public/images/${req.params.id}`))
+        res.sendFile(resolve(`./public/images/${req.params.id}`))
     });
 
     app.get("/js/select_character.js", (req, res, next) => {
-        res.sendFile(resolve('./app/public/js/select_character.js'))
+        res.sendFile(resolve('./public/js/select_character.js'))
     });
 
     app.get('/css/:id', (req, res, next) => {
-        res.sendFile(resolve(`./App/public/css/${req.params.id}`));
+        res.sendFile(resolve(`./public/css/${req.params.id}`));
     });
 
     app.get('/js/:id', (req, res, next) => {
-        res.sendFile(resolve(`./App/public/js/${req.params.id}`));
+        res.sendFile(resolve(`./public/js/${req.params.id}`));
     });
 
     app.get('/images/Wood_Texture_Pack/:id', (req, res, next) => {
-        res.sendFile(resolve(`./app/public/images/Wood_Texture_Pack/${req.params.id}`))
+        res.sendFile(resolve(`./public/images/Wood_Texture_Pack/${req.params.id}`))
     });
 
     app.get('/socket.io.js', (req, res, next) => {
-        res.sendFile(resolve('./App/api/node_modules/socket.io/client-dist/socket.io.min.js'));
+        res.sendFile(resolve('./api/node_modules/socket.io/client-dist/socket.io.min.js'));
     });
 
     app.get('/socket.io.min.js.map', (req, res, next) => {
-        res.sendFile(resolve('./App/api/node_modules/socket.io/client-dist/socket.io.min.js.map'));
+        res.sendFile(resolve('./api/node_modules/socket.io/client-dist/socket.io.min.js.map'));
     });
 
     app.get('/images/unidentified.png', (req, res, next) => {
-        res.sendFile(resolve('./App/public/images/player_placeholder.png'));
+        res.sendFile(resolve('./public/images/player_placeholder.png'));
     });
 
     // app.get('/users/signup', (req, res, next) => {
-    //     res.sendFile(resolve('./App/public/html/signup.html'));
+    //     res.sendFile(resolve('./public/html/signup.html'));
     // });
 
     // app.get('/js/Game_Logic.js', (req, res, next) => {
-    //     res.sendFile(resolve('./App/public/js/Game_Logic.js'));
+    //     res.sendFile(resolve('./public/js/Game_Logic.js'));
     // });
 
     // app.get('/js/Player_Controller.js', (req, res, next) => {
-    //     res.sendFile(resolve('./App/public/js/Player_Controller.js'));
+    //     res.sendFile(resolve('./public/js/Player_Controller.js'));
     // });
 
     // app.get('/js/signup.js', (req, res, next) => {
-    //     res.sendFile(resolve('./App/public/js/signup.js'));
+    //     res.sendFile(resolve('./public/js/signup.js'));
     // });
 
     app.use((req, res, next) => {
